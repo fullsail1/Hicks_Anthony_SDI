3620f00e48871faa8cb4fe851920a180918c1678
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-01-13 18:28:19 -0500

    avengers update
    day 4

--- day 4/day4.js
+++
@@ -1,9 +0,0 @@
-/*
-Anthony Hicks
-SDI Campus
-1/13/2015
-Day 4 In Class Assignment
-Arrays
- */
-
-alert("test");
---
+++ day 4/js/day4.js
@@ -0,0 +1,41 @@
+/*
+Anthony Hicks
+SDI Campus
+1/13/2015
+Day 4 In Class Assignment
+Arrays
+ */
+
+//alert("test");
+
+//create a basic array
+var avengersNames = ["thor", "captain america", "iron man"];
+
+//print it out to the console
+console.log(avengersNames);
+
+//print specific
+console.log(avengersNames[1]);
+
+//switch a team member iron man for spiderman
+avengersNames[2] = "spiderman";
+console.log(avengersNames);
+
+//add a team member
+avengersNames[3] = "blackwidow";
+console.log(avengersNames);
+
+
+//use a variable as an index number
+var num = 2;
+console.log(avengersNames[num]);
+
+//property of array - length property
+
+//.syntax - fancy for use a period
+avengersNames(avengersNames.length) = "powerman";
+console.log(avengersNames);
+console.log(avengersNameslength);
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             mnnn
