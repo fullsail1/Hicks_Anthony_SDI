81355a9a314c9e57d2798d70df817782cce7b24a
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-01-13 19:57:12 -0500

    prompting
    day 4

--- day 4/js/day4_prompting.js
+++ day 4/js/day4_prompting.js
@@ -4,5 +4,26 @@ SDI Campus
 1/13/2015
 Prompting
  */
-alert("testing again");
+//alert("testing again");
 
+//ask user for input - prompt()
+//save the user response - by creating variable
+var userInput = prompt("Enter your year of birth");
+console.log(userInput);
+
+//calculate the area of a rectangle
+// area = length * width
+
+//create 2 variables for length and width
+
+var length = prompt("enter length");
+var width = prompt("enter width");
+
+// calculate area
+
+var area = length*width
+console.log(area);
+
+//
+var areaRect = length*width
+console.log("the area of your rectangle with a length of " + length + " and a width of " + width + " is " + areaRect);
