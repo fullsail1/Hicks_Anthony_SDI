33753f8762458177f69ea8f3ac13e49be641f490
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-01-13 19:06:25 -0500

    learned push pop splice
    day 4

--- day 4/js/day4.js
+++ day 4/js/day4.js
@@ -37,5 +37,30 @@ avengersNames(avengersNames.length) = "powerman";
 console.log(avengersNames);
 console.log(avengersNameslength);
 
+//pick oranges abd track how may did weapons
+//
+/method of Arrays
+//push - insert an item at the end of array
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             mnnn
+fruitbowl.push("APPLE");
+console.log(fruitbowl);
+//pop- takes off the last item in an array
+
+var caught = fruitbowl.pop();
+
+
+
+//SPLICE - IS USED FOR REMOVING OR ADDING ITEMS INTO AN EXISTING ARRAY
+
+fruitbowl.splice[1,0,"banana"];
+console.log(fruitbowl);
+
+fruitbowl.splice[1,2,"gauva"];
+
+fruitbowl.splice(1,1,"babboa"); //start at 1, remove number of items - 1, replace that item with "babboa"
+
+fruitbowl.splice(1,0,"peach", "coconut","rasberry" );
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 mnnn
