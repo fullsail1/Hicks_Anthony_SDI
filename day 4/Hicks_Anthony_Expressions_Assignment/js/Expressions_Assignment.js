f887d2acffc376589e529e177fbea74ddf247f7b
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-01-15 14:51:23 -0500

    array updates
    array arithmetic

--- day 4/Hicks_Anthony_Expressions_Assignment/js/Expressions_Assignment.js
+++ day 4/Hicks_Anthony_Expressions_Assignment/js/Expressions_Assignment.js
@@ -13,27 +13,44 @@ Expressions_Assignment
 
 var helmetsIssued;
 var bikeRentalFee = 5;
-var addedEquip = 5;
+var vestIssued = 5;
 var lateReturnFee = 4;
 var equipmentArray = [40, 40, 40, 0, 0, 0]; //Bikes In Stock, Helmets In Stock, Vests In Stock
 var totalEquipIssued;
 var totalEquipCost;
 
+//prompts
 
 bikeRented = parseInt(prompt("how many bikes being rented?"));
  console.log(bikeRented);
+ equipmentArray[3]= bikeRented;
 
  helmetsIssued = parseInt(prompt("How many helmets do you need?"));
  console.log(helmetsIssued);
-addedEquip =parseInt(prompt("How many vests do you need?"));
-console.log(addedEquip);
+ equipmentArray[4]= helmetsIssued;
+
+vestIssued =parseInt(prompt("How many vests do you need?"));
+console.log(vestIssued);
+ equipmentArray[5]= vestIssued;
 
  //calculations
- totalEquipIssued = (bikeRented) + (helmetsIssued) + (addedEquip);
+ //bike shop audits
+
+
+
+ totalEquipIssued = (bikeRented) + (helmetsIssued) + (vestIssued);
  console.log(totalEquipIssued);
- totalEquipCost = 5*(bikeRented) + 5*(addedEquip);
+ equipmentArray[6]=totalEquipIssued;
+
+ totalEquipCost = 5*(bikeRented) + 5*(vestIssued);
  console.log(totalEquipCost);
- 
+ equipmentArray[7]= totalEquipCost;
+
+ //update array
+equipmentArray[0]= equipmentArray[0]- bikeRented;
+ equipmentArray[1]= equipmentArray[1]-helmetsIssued;
+ equipmentArray[2]= equipmentArray[2]-vestIssued
+ console.log(equipmentArray);
 
 
 
