13b73f8e97478d97fe81c2b117944c62e4c6c0a9
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-01-15 19:18:57 -0500

    relational expression
    conditionals

--- day5/js/day5_conditional.js
+++ day5/js/day5_conditional.js
@@ -7,3 +7,32 @@ conditional
  */
 //alert("testing again");
 
+// basic conditional statement
+
+ //create boolean varible to test
+
+ var oldEnough = true
+
+ //if child is old enough the print to console
+
+ if(oldEnough){
+  //code inside of {} will run if it is true
+  //this code will be skipped if test is false
+  console.log("you can ride the coaster!");
+  }
+
+ console.log("what comes after the if statement?");
+
+ //relational expression
+
+ //if the kid is over 48" tall, then he can ride
+ var kidHeight = 46;
+
+ //create variable for mininumright
+
+ if(kidHeight > 48){
+  console.log("you are tall enough to ride!");
+ }else{
+  //this code will run if the test is false!
+  console.log("sorry, you are too short");
+ };
