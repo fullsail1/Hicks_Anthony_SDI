1b49b8dedd403673f4fc5d418c9b39ed4911a6bd
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-01-15 20:52:42 -0500

    else if statements + logical operator
    multiple statements

--- day5/js/day5_conditional.js
+++ day5/js/day5_conditional.js
@@ -28,11 +28,53 @@ conditional
  //if the kid is over 48" tall, then he can ride
  var kidHeight = 46;
 
+
  //create variable for mininumright
 
+ var minHeight = 48;
+
+ //create a variable for show lift
+ var shoeLift = 2;
+ // is the kid tall enough with insert?
+
+ if(kidHeight + shoelift > minheight)
  if(kidHeight > 48){
   console.log("you are tall enough to ride!");
  }else{
   //this code will run if the test is false!
   console.log("sorry, you are too short");
  };
+
+
+
+
+
+
+
+ var childHeight = s0;
+ if(childHeight>48){
+  console.log("you can ride space mountain!");
+
+ }else if(childHeight >= 40){
+  console.log("You can ride Buzz Lightyear and winnie the pooh");
+
+ }else{
+
+  console.log("you can only ride Winnie the pooh"];
+
+ }
+ /*
+ //if, else if, else statement
+
+ //if(conditional to test){
+ code to run if conditional1 is true.
+ } else if(condition2 to test){
+ condition2 will only be testing if the conditional is false!
+ code to run if condition2 is true
+ }else{
+ code to run if condition1 and condition are false
+ }
+
+ */
+
+
---
+++ day5/js/day5_logical_operators.js
@@ -0,0 +1,53 @@
+
+ /*
+Anthony Hicks
+SDI Campus
+1/15/2015
+Logical Operators
+ */
+//alert("testing again");
+
+/*
+true&&true = true
+true&&false= false
+false&&true= false
+false&&false=false
+
+true||true = true
+true||false= true
+false||true = true
+false||false = false
+
+!true = false
+!false = true
+
+
+ */
+
+ // if the price of 3ds is less than budget lets buy it
+ //and if our paycheck is over 500 dollars
+
+ var budget = 100;
+ var dsPrice = 169;
+ var paycheck = 600;
+
+ if(dsPrice<budget && paycheck > 500){
+  console.log("Buy the 3ds");
+
+
+ }else{
+  console.log("Do not buy, save a little more");
+ }
+
+
+ var tvPrice = 540;
+ var tvBudget = 500;
+ var wonLottery = false;
+
+ if(tvPrice>tvBudget||wonLottery){
+  console.log("lets go buy tv");
+
+ }else{
+  console.log("stick with your small tv!");
+  
+ }
