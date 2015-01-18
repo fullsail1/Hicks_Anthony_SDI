e032d07809a6bfbc75fcf94126d9b431acd997eb
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-01-17 20:21:10 -0500

    math object stuff
    floor, ceil, round

--- Day 6/js/rename-me.js
+++ Day 6/js/rename-me.js
@@ -26,3 +26,27 @@ if (airtemp>= 85) {
     
 }
 
+//round
+//normal rounding .5 and up
+
+var num1 = 9.54444;
+console.log(num1);
+num1 = Math.round(num1);
+console.log(num1);
+
+//floor
+//returns x, rounded down to the nearest integer/ wholenumber
+var num2 = 6.1;
+console.log(num2);
+num2= Math.floor(num2);
+console.log(num2);
+
+//ceil(x)
+//return x rounded up to the next integer/whole number
+var num3 = 4.8888;
+console.log(num3);
+num3 = Math.ceil(num3);
+console.log(num3);
+
+
+
