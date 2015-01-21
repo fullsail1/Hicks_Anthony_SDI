06ea1f6102ff7ad83503de24ffff4166c6159d62
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-01-21 00:02:24 -0500

    Conditionals assignment
    Redoing assignment commit 1

--- Day 7/js/Day7_validatingprompts.js
+++ Day 7/js/Day7_validatingprompts.js
@@ -1,1 +1,74 @@
-alert("yup");
+//alert("yup");
+/*
+var userInput = prompt("please type in your name.")
+
+//validitating a prompt with a conditional.
+if (userInput===" ") {
+    //code
+    //user did not type anything in
+    userInput= prompt("Please don't leave it blank. Type in your name.");
+    
+}
+
+//instead use a while loop!
+//check the input multiple times till it changes
+
+var firstName = prompt("Please type your First name");
+
+while (firstName===" ") {
+    //code
+    console.log(" Please do not enter blank..")
+}
+
+//Test to see if it is a number
+//isNan() - test ii
+}
+*/
+var userInput=prompt("Please type in your name");
+
+//Validating a prompt with a conditional
+
+if (userInput==="") {
+    //User did not type anything in
+    userInput=prompt("Please do not leave blank. Type in your name.");
+}
+
+//Instead, use a while loop
+//Check the input multiple times as many as it takes
+
+var firstName=prompt("Please type in your first name:");
+
+while (firstName==="") {
+    //The user typed in nothing, re-prompt for first name
+    firstName=prompt("Please type in your first name and DO NOT leave this blank.");
+}
+
+
+//Test to see if it is a number
+//isNaN() - tests what is inside of the (). Is it not a number?
+//Numbers return false
+//Everything else returns true
+
+console.log(isNaN("Monkey")); //Returns true
+console.log(isNaN(7)); //Returns false
+
+//Lets ask the user their age
+
+var age=prompt("Please enter your age:");
+
+//Validate that the user typed in a number
+while(isNaN(age)||age ===""){
+    //This runs anytime that the age is not a number
+    //Reprompt the user for a number
+    age=prompt("Only use numbers, please enter your age:");
+}
+
+var choice=prompt("Please type in yes or no:");
+
+//Convert to lower case
+
+choice=choice.toLowerCase();
+
+while(choice!="yes" &&choice!="no"){
+    choice=prompt("Please only type in yes or no.");
+}
---
+++ Hicks_Anthony_Conditionals_Assignment/js/Conditonals_Assignment.js
@@ -0,0 +1,39 @@
+
+ /*
+Anthony Hicks
+SDI Campus
+1/20/2015
+Conditional Assignment
+ */
+//alert("testing again");
+
+ //Soldier promotion point system
+
+ //variables
+ /*
+ var awards;
+ var ptTest;
+ var weaponsQual;
+ var educationPoints;
+ var totalPoints;
+ var promoArray = [];
+
+ // calculate points
+
+
+
+
+
+
+
+
+
+console.log( "soldier points are as follows : " + promoArray );
+
+
+
+
+
+console.log(" soldier meets minimum " +  totalPoints + " for promotion. Send to board "
+console.log("soldier does not meet minimum " + totalPoints + " for promotion"
+  */
