d62855996a5d8f1ea069a5b3c6dc8699fc9f697c
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-01-22 23:34:32 -0500

    prompting the user
    the take

--- Day8/js/day8_return.js
+++ Day8/js/day8_return.js
@@ -4,4 +4,76 @@ SDI Campus
 1/13/2015
 Day 8 return
  */
-alert("testing again");
+//alert("testing again");
+
+
+
+//create a function calculating area of a rectangle
+
+function calcArea(w,h) {
+    //code
+    var area = w*h;
+    console.log(area);
+    
+}
+
+//function call
+calcArea(77,9);
+
+function calcArea(w,h) {
+    //code
+    var area = w*h;
+    console.log(area);
+    return area;
+    
+}
+
+var returnedArea = calcArea(77,9);
+
+console.log(returnedArea);
+
+
+
+//create a function to calculate the area of a circle
+
+//area = pi*r*r
+function circleArea(radius) {
+    //code
+    var area = Math.PI*radius*radius;
+    //return area
+    return area
+}
+
+//function call
+circleArea(6);
+var result = circleArea(6);
+
+console.log("the area of the circle is " + result);
+
+var twoArea = result * 2;
+console.log(" the area of both circles is " + twoArea.toFixed(0));
+
+var result2 = circleArea(3);
+console.log(" the area of a circle with a radius of 3 is " + result2);
+
+//prompt user for radius. Calc area
+var userRad = prompt(" Please type in a radius: ");
+
+//validate prompt
+while(isNaN(userRad)|| userRad===""){
+    userRad=prompt("Please only use numbers\nType in a radius");
+}
+
+//parse the variable
+userRad = parseInt(userRad);
+
+//create a variable to catch the results!
+var userResults = circleArea(userRad);
+
+console.log(" The area of your circle with a radius of " + userRad + " is " + userResults.toFixed(1));
+
+
+
+
+
+ 
---
+++ Functions_worksheet/js/functions_worksheet.js
@@ -0,0 +1,36 @@
+ /*
+Anthony Hicks
+SDI Campus
+1/22/2015
+functions worksheet
+ */
+//alert("testing again");
+
+//create function
+//car lot wants to know how much the daily rental take is
+
+//think about it......
+
+
+
+function dailyTake(cf, rf )
+{//cars rented , rental fee
+    var cf;
+    var rf;
+    var take;
+    
+    take = cf * rf //calculation
+    //meaningful output
+    console.log("the daily take a the lot down 4th st. is " + take + " dollars");
+    
+    
+    
+    
+}
+
+dailyTake(198, 60 );//call your function and insert the arguments
+
+//prompt user
+
+var cf = ParseInt(prompt("Please enter daily rental tally sheet"))
+var rf = parseInt(prompt("enter you rental payment"))
