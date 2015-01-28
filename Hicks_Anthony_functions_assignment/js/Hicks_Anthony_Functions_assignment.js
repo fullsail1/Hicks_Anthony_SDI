479cb73cd01b47bea526eec396dcdd02bb523022
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-01-28 18:00:49 -0500

    having trouble with anonymous function
    have to tweak the else if statements

--- Hicks_Anthony_functions_assignment/js/Hicks_Anthony_Functions_assignment.js
+++ Hicks_Anthony_functions_assignment/js/Hicks_Anthony_Functions_assignment.js
@@ -38,29 +38,39 @@ var results = (Math.round(bodyFatCalc(weight,height)));
 console.log( " Our analysis indicates you BMI is " + results);
 
 
-var bmiResults = function bmiColor(weight, height, age){
+var age = prompt("Enter your age.");
+while(age === ""||age === isNaN(age)){
+    
+    //reprompt
+    prompt("Please DO NOT USE - Words and be sure to use numbers\nEnter your age. " );
+  
+}
+
+var bmiResults = function (w, h, age){
    
    var bmi = 703*(w/(h*h));
-    if (age >= 0  || age<=27 && bmi>50) {
+    if (age >= 0  || age<=27 && bmi<15||bmi<=19) {
         //code
-        console.log("  Invisible.    Your BMI indicates that you should call a priest. " )
+        console.log("  Green.    Your BMI of " + bmi + " indicates that you are in good standing. Eat more green leafy veggies. " )
       
-    } else if (age >= 28||age <=45 && bmi > 60) {
+    } else if (age >=28 ||age<=45 && bmi >=20|| bmi <=32) {
             //code
              console.log( " CYAN.  Your BMI indicates you should eat more Apples & BAILLALQUH fruit!");
         }else
         
         console.log("  Invisible.    Your BMI indicates that you should call a priest. " )
     
-    return bmiResults ;
+    return;
 }
 
+var results2 = function (weight,height,age){
 
-var results2= function bmiColor(bmiResults){
+console.log()
 
 }
+bmiResults(weight, height, age);
+//console.log(bmiResults);
 
-}
 
 
 
