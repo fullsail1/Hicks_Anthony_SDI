01bbf1ff214b29c1f00c348fbb546c2bc9cb0c26
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-01-28 00:50:30 -0500

    troubleshooting anonymous function
    adding conditionals

--- Hicks_Anthony_functions_assignment/js/Hicks_Anthony_Functions_assignment.js
+++ Hicks_Anthony_functions_assignment/js/Hicks_Anthony_Functions_assignment.js
@@ -34,9 +34,33 @@ while(height === ""||height === isNaN(height)){
 
 
 
-
 var results = (Math.round(bodyFatCalc(weight,height)));
 console.log( " Our analysis indicates you BMI is " + results);
+
+
+var bmiResults = function bmiColor(weight, height, age){
+   
+   var bmi = 703*(w/(h*h));
+    if (age >= 0  || age<=27 && bmi>50) {
+        //code
+        console.log("  Invisible.    Your BMI indicates that you should call a priest. " )
+      
+    } else if (age >= 28||age <=45 && bmi > 60) {
+            //code
+             console.log( " CYAN.  Your BMI indicates you should eat more Apples & BAILLALQUH fruit!");
+        }else
+        
+        console.log("  Invisible.    Your BMI indicates that you should call a priest. " )
+    
+    return bmiResults ;
+}
+
+
+var results2= function bmiColor(bmiResults){
+
+}
+
+}
 
 
 
