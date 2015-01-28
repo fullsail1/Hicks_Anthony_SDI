7659cb7200d986cf7b3036f0438dd328be73d511
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-01-28 00:04:31 -0500

    adding anonymous
    finishing first function

--- Hicks_Anthony_functions_assignment/js/Hicks_Anthony_Functions_assignment.js
+++ Hicks_Anthony_functions_assignment/js/Hicks_Anthony_Functions_assignment.js
@@ -5,16 +5,38 @@ SDI Campus
 1/13/2015
 Hicks_Anthony_Functions_assignment
  */
-alert("testing again");
+//alert("testing again");
 
 
 function bodyFatCalc(w,h){
     
-var w ;
-var h ;
 var bmi = 703*(w/(h*h));
 
 return bmi;
 }
-console.log(Math.round(bodyFatCalc(232,68)));
+
+var weight = prompt("Enter your weight.");
+while(weight === ""||weight === isNaN(weight)){
+    
+    //reprompt
+    prompt("Please DO NOT USE - Words and be sure to use numbers\nEnter your weight. " );
+  
+}
+
+var height = prompt("Enter your height.");
+while(height === ""||height === isNaN(height)){
+    
+    //reprompt
+    prompt("Please DO NOT USE - Words and be sure to use numbers\nEnter your height. " );
+    
+}
+
+
+
+
+
+var results = (Math.round(bodyFatCalc(weight,height)));
+console.log( " Our analysis indicates you BMI is " + results);
+
+
 
--- notesreview/js/notesreview.js
+++ notesreview/js/notesreview.js
@@ -1,39 +1,11 @@
 /*
 Anthony Hicks
 SDI Campus
-1/24/2015
-multiple
+1/27/2015
+notesreview
  */
 //alert("testing again");
-/*
-var results2 = triArea(6,7);
-console.log(results2);
 
-function triArea(b,h) {
-    //code
-    //area = 1/2 * base * height
-    var area = .5 * b * h;
-    return area
-}
-/*
-THIS DOES NOT WORK
-FUNCTION CALL HAS TO COME AFTER ANON FUNCTION DEFINITION
-var results4 = triAreaAnon(8,9);
-
-
-//call function
-var results1 = triArea(7,7);
-console.log(results1);
-
-//create an anonymous function that calc the area of triangle
-var triAreaAnon = function(b,h){
-     var area = .5 * b * h;
-    return area
-    
-}
-var results3 = triAreaAnon(4,5);
-console.log("results of anonymous function after defined is " + results3 );
-*/
 
 //calculate how much pizza costs per square inch and per slice.
 
@@ -107,4 +79,3 @@ function pricePerSlice(price, slice) {
     return costPerSlice;
 
 }
-
