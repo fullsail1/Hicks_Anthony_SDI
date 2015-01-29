c0e5a5872a09dfa1071d23da0b01c6c8b3eb8f23
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-01-29 14:44:46 -0500

    finished
    done

--- Hicks_Anthony_functions_assignment/js/Hicks_Anthony_Functions_assignment.js
+++ Hicks_Anthony_functions_assignment/js/Hicks_Anthony_Functions_assignment.js
@@ -7,6 +7,7 @@ Hicks_Anthony_Functions_assignment
  */
 //alert("testing again");
 
+// create function to calculate body fat
 
 function bodyFatCalc(w,h){
     
@@ -15,31 +16,37 @@ var bmi = 703*(w/(h*h));
 return bmi;
 }
 
-var weight = prompt("Enter your weight.");
+//create prompts for arguments
+//weight & height
+
+var weight = parseInt(prompt("Enter your weight."));
+//validate
 while(weight === ""||weight === isNaN(weight)){
     
     //reprompt
     prompt("Please DO NOT USE - Words and be sure to use numbers\nEnter your weight. " );
   
 }
-
-var height = prompt("Enter your height.");
+9
+var height = parseInt(prompt("Enter your height in inches."));
+//validate
 while(height === ""||height === isNaN(height)){
     
     //reprompt
-    prompt("Please DO NOT USE - Words and be sure to use numbers\nEnter your height. " );
+    prompt("Please DO NOT USE - Words and be sure to use numbers\nEnter your height in inches. " );
     
 }
 
 
-
+// validation made us add Math.round  
 
 var results = (Math.round(bodyFatCalc(weight,height)));
 console.log( " Our analysis indicates you BMI is " + results);
 
+//prompt for age
 
-var age = prompt("Enter your age.");
-while(age === ""||age === isNaN(age)){
+var age = parseInt(prompt("Enter your age."));
+while(age === ""||age === isNaN(age)|| age<0 || age >71){ //validate age prompt
     
     //reprompt
     prompt("Please DO NOT USE - Words and be sure to use numbers\nEnter your age. " );
@@ -47,31 +54,20 @@ while(age === ""||age === isNaN(age)){
 }
 
 var bmiResults = function (w, h, age){
-   
+   //we want to add age in the mix saying that bmi compared to age will yeild a result of normal, high or crazy
    var bmi = 703*(w/(h*h));
-   
-    if (age >= 0  || age<=27 && bmi<15||bmi<=19) {
-        //code
-        console.log("  Green.    Your BMI of " + bmi + " indicates that you are in good standing. Eat more green leafy veggies. " )
-      
-    } else if (age >=28 ||age<=45 && bmi >=20|| bmi <=24) {
-            //code
-             console.log( " CYAN.  Your BMI of " + bmi +  " indicates you should eat more Apples & BAILLALQUH fruit!");
-        }else
-        
-        console.log("  Invisible.    Your BMI indicates that you should call a priest. " )
-    
-    return ;
+   var fat= (1.20*bmi)+(0.23*age)-5.4;
+
+    return fat;
 }
 
-var results2 = function (weight,height,age){
+var results2 = Math.round(bmiResults(weight,height,age));
+console.log(" Our analysis indicates you Body Fat Percentage is " + results2 +"%");
 
-console.log()
 
-}
+
 bmiResults(weight, height, age);
 //console.log(bmiResults);
-
 
 
 
